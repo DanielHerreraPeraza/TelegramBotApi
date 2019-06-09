fbee65475af7a744c158eb5a21b954171f7a3a83
Daniel José Herrera Peraza <danieljhp@gmail.com>
2019-06-08 18:19:04 -0600

    Add TelegramBotExamples

---
+++ TelegramBotExamples/TelegramBotExamples/Program.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace TelegramBotExamples
+{
+    class Program
+    {
+        //Key del bot
+        private static readonly TelegramBotClient Bot = new TelegramBotClient("766532439:AAHIOhVEzv4GjYwXR1-2_tfBl2J8gcE-YM4");
+
+        static void Main(string[] args)
+        {
+            //Método que se ejecuta cuando se recibe un mensaje
+            Bot.OnMessage += BotOnMessageReceived;
+
+            //Método que se ejecuta cuando se recibe un callbackQuery
+            Bot.OnCallbackQuery += BotOnCallbackQueryReceived;
+
+            //Método que se ejecuta cuando se recibe un error
+            Bot.OnReceiveError += BotOnReceiveError;
+
+            //Inicia el bot
+            Bot.StartReceiving();
+            Console.WriteLine("Bot levantado!");
+            Console.ReadLine();
+            Bot.StopReceiving();
+        }
+
+        private static async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
+        {
+            var message = messageEventArgs.Message;
+
+            if (message == null || message.Type != MessageType.Text) return;
+            
+            switch (message.Text.Split(' ').First())
+            {
+                //Enviar un inline keyboard con callback
+                case "/parte1":
+
+                    //Simula que el bot está escribiendo
+                    await Bot.SendChatActionAsync(message.Chat.Id, ChatAction.Typing);
+
+                    await Task.Delay(50);
+
+                    var keyboardEjemplo1 = new InlineKeyboardMarkup(new[]
+                    {
+                    new []
+                    {
+                        InlineKeyboardButton.WithCallbackData(
+                            text:"Imagen",
+                            callbackData: "imagen"),
+                        InlineKeyboardButton.WithCallbackData(
+                            text:"Ubicación",
+                            callbackData: "ubicacion"),
+                    },
+                    new []
+                    {
+                        InlineKeyboardButton.WithCallbackData(
+                            text:"Ubicación + info",
+                            callbackData: "venue"),
+                        InlineKeyboardButton.WithCallbackData(
+                            text:"Contacto",
+                            callbackData: "contacto"),
+                    },
+                    new []
+                    {
+                        InlineKeyboardButton.WithCallbackData(
+                            text:"Animación",
+                            callbackData: "animation"),
+                        InlineKeyboardButton.WithCallbackData(
+                            text: "Documento",
+                            callbackData: "document"),
+                    }
+                });
+
+                    await Bot.SendTextMessageAsync(
+                        message.Chat.Id,
+                        "Elija una opción",
+                        replyMarkup: keyboardEjemplo1);
+                    break;
+
+                case "/parte2":
+
+                    var keyboardEjemplo2 = new InlineKeyboardMarkup(new[]
+                    {
+                    new []
+                    {
+                        InlineKeyboardButton.WithCallbackData(
+                            text:"Keyboard",
+                            callbackData: "keyboard"),
+                        InlineKeyboardButton.WithCallbackData(
+                            text:"Reply",
+                            callbackData: "reply"),
+                    },
+                    new []
+                    {
+                        InlineKeyboardButton.WithCallbackData(
+                            text:"Reenviar",
+                            callbackData: "reenviar"),
+                        InlineKeyboardButton.WithCallbackData(
+                            text:"Force reply",
+                            callbackData: "forceReply"),
+                    },
+                    new []
+                    {
+                        InlineKeyboardButton.WithCallbackData(
+                            text:"Texto con formato",
+                            callbackData: "formato"),
+                        InlineKeyboardButton.WithCallbackData(
+                            text:"Video",
+                            callbackData: "video"),
+                    }
+                });
+
+                    await Bot.SendTextMessageAsync(
+                        message.Chat.Id,
+                        "Elija una opción",
+                        replyMarkup: keyboardEjemplo2);
+                    break;
+                
+                //Mensaje por default
+                default:
+                    const string usage = @"
+                Comandos:
+                /parte1 - ejemplos parte 1
+                /parte2 - ejemplos parte 2";
+
+                    await Bot.SendTextMessageAsync(
+                        message.Chat.Id,
+                        text: usage,
+                        replyMarkup: new ReplyKeyboardRemove());
+
+                    break;
+            }           
+        }
+
+        private static async void BotOnCallbackQueryReceived(object sender, CallbackQueryEventArgs callbackQueryEventArgs)
+        {
+            var callbackQuery = callbackQueryEventArgs.CallbackQuery;
+
+            switch (callbackQuery.Data)
+            {
+                case "keyboard":
+                    ReplyKeyboardMarkup tipoContacto = new[]
+                    {
+                        new[] { "Opción 1", "Opción 2" },
+                        new[] { "Opción 3", "Opción 4" },
+                    };
+
+                    await Bot.SendTextMessageAsync(
+                        chatId: callbackQuery.Message.Chat.Id,
+                        text: "Keyboard personalizado",
+                        replyMarkup: tipoContacto);
+                    break;
+
+                case "ubicacion":
+                    await Bot.SendLocationAsync(
+                        chatId: callbackQuery.Message.Chat.Id,
+                        latitude: 9.932551f,
+                        longitude: -84.031086f
+                        );
+                    break;
+
+                case "venue":
+                    await Bot.SendVenueAsync(
+                        chatId: callbackQuery.Message.Chat.Id,
+                        latitude: 9.932551f,
+                        longitude: -84.031086f,
+                        title: "Cenfotec",
+                        address: "San José, Montes de Oca"
+                        );
+                    break;
+
+                case "imagen":
+                    await Bot.SendPhotoAsync(
+                        chatId: callbackQuery.Message.Chat.Id,
+                        photo: "https://www.google.co.cr/imgres?imgurl=https%3A%2F%2Fwww.pcactual.com%2Fmedio%2F2017%2F07%2F05%2Ftelegram_960x540_0aa1aeac.jpg&imgrefurl=https%3A%2F%2Fwww.pcactual.com%2Fnoticias%2Factualidad%2Fsoy-fan-telegram_13549&docid=UZhcuJ9275t8zM&tbnid=otB1G_5L3DD0sM%3A&vet=10ahUKEwjR0ouWotDiAhUiqlkKHdi6D8gQMwhLKAEwAQ..i&w=960&h=540&bih=722&biw=1536&q=telegram%20image&ved=0ahUKEwjR0ouWotDiAhUiqlkKHdi6D8gQMwhLKAEwAQ&iact=mrc&uact=8"
+                        );
+                    break;
+
+                case "animation":
+                    await Bot.SendAnimationAsync(
+                        chatId: callbackQuery.Message.Chat.Id,
+                        animation: "https://techcrunch.com/wp-content/uploads/2015/08/safe_image.gif?w=730&crop=1"
+                        );
+                    break;
+
+                case "video":
+                    await Bot.SendVideoAsync(
+                        chatId: callbackQuery.Message.Chat.Id,
+                        video: "https://res.cloudinary.com/dherrerap/video/upload/v1560039252/WhatsApp_Video_2019-06-08_at_6.10.54_PM.mp4"
+                        );
+                    break;
+
+                case "document":
+                    await Bot.SendDocumentAsync(
+                        chatId: callbackQuery.Message.Chat.Id,
+                        document: "https://cenfotec.s3-us-west-2.amazonaws.com/prod/wpattchs/2013/04/web-tec-virtual.pdf"
+                        );
+                    break;
+
+                case "formato":
+                    await Bot.SendTextMessageAsync(
+                        chatId: callbackQuery.Message.Chat.Id,
+                        text: "<b>bold</b>, <strong>bold</strong>",
+                        parseMode: ParseMode.Html
+                        );
+                    await Bot.SendTextMessageAsync(
+                        chatId: callbackQuery.Message.Chat.Id,
+                        text: "<i>italic</i>, <em>italic</em>",
+                        parseMode: ParseMode.Html
+                        );
+                    await Bot.SendTextMessageAsync(
+                        chatId: callbackQuery.Message.Chat.Id,
+                        text: "<a href='http://www.example.com/'>inline URL</a>",
+                        parseMode: ParseMode.Html
+                        );
+                    break;
+
+                case "reply":
+                    await Bot.SendTextMessageAsync(
+                        chatId: callbackQuery.Message.Chat.Id,
+                        text: "ID: " + callbackQuery.Message.MessageId + " - " + callbackQuery.Message.Text,
+                        replyToMessageId: callbackQuery.Message.MessageId);
+                    break;
+
+                case "contacto":
+                    await Bot.SendContactAsync(
+                        chatId: callbackQuery.Message.Chat.Id,
+                        phoneNumber: "2222-2222",
+                        firstName: "Jane",
+                        lastName: "Doe"
+                        );
+                    break;
+
+                case "forceReply":
+                    await Bot.SendTextMessageAsync(
+                        chatId: callbackQuery.Message.Chat.Id,
+                        text: "Forzar respuesta a este mensaje",
+                        replyMarkup: new ForceReplyMarkup());
+                    break;
+
+                case "reenviar":
+                    await Bot.ForwardMessageAsync(
+                        chatId: callbackQuery.Message.Chat.Id,
+                        fromChatId: callbackQuery.Message.Chat.Id,
+                        messageId: callbackQuery.Message.MessageId
+                        );
+                    break;
+            }
+        }
+
+        private static void BotOnReceiveError(object sender, ReceiveErrorEventArgs receiveErrorEventArgs)
+        {
+            Console.WriteLine("Received error: {0} — {1}",
+                receiveErrorEventArgs.ApiRequestException.ErrorCode,
+                receiveErrorEventArgs.ApiRequestException.Message);
+        }
+    }
+}
